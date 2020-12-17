33003e6454c7ef89081984aa896cf9c2dfb6120f
Leon <leonrubner2809@gmail.com>
2020-12-17 12:23:57 +0100

    stated with schemas and resolvers

---
+++ graphql/resolvers.js
@@ -0,0 +1,8 @@
+const User = require('../models/user');
+
+module.exports = {
+    createUser({ userInput }, req) {
+        const email = userInput.email
+        /*const email = args.userInput.email;*/
+    }
+}
